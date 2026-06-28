78e9f056a3c65166c3989b15094f0314c3e7e220
ShikoShintaro <shikoshintaro21@gmail.com>
2026-06-28 23:11:37 +0800

    New Schema added

---
+++ models/LedgerEntry.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const ledgerEntrySchema = new mongoose.Schema(
+    {
+        type : {
+            type : String,
+            enum : ["adoption", "donation", "foster", "animal", "system"],
+            default : "system",
+        },
+
+        action : {
+            type : String,
+            required : true,
+        },
+
+        actorName : String,
+        actorEmail : String,
+
+        targetType : String,
+        targetId : String,
+
+        description : {
+            type : String,
+            required : true,
+        },
+
+        status : String,
+
+        amount : {
+            type : Number,
+            default : 0,
+        },
+
+        metadata : {
+            type : mongoose.Schema.Types.Mixed,
+            default : {},
+        },
+    },
+    { timestamps : true }
+);
+
+module.exports = mongoose.model("ledgerEntry", ledgerEntrySchema);
