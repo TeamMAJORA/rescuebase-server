8f03b26a71c1c1aa69eedaf0cb459cdb264e7524
ShikoShintaro <shikoshintaro21@gmail.com>
2026-06-28 22:59:36 +0800

    Test

--- routes/adoptionRoutes.js
+++ routes/adoptionRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const AdoptionApplication = require("../models/AdoptionApplication");
 
+
+//The main
 router.post("/", async (req, res) => {
     try {
         const application = await AdoptionApplication.create({
