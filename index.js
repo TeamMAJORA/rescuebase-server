5d302bf948438604db1a064d96154dd495615a13
ShikoShintaro <shikoshintaro21@gmail.com>
2026-07-15 20:52:12 +0800

    import of upload images API

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const ledgerRoutes = require("./routes/ledgerRoutes");
 const fosterRoutes = require("./routes/fosterRoutes");
 const userRoutes = require("./routes/userRoutes");
 const donationRoutes = require("./routes/donationRoutes");
-const animalRoutes = require("./routes/animalRoutes")
+const animalRoutes = require("./routes/animalRoutes");
+
+//Upload to cloudinary
+const uploadRoutes = require("./routes/uploadRoutes");
 
 const app = express();
 
@@ -56,6 +59,9 @@ app.use("/api/foster", fosterRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/donations", donationRoutes);
 app.use("/api/animals", animalRoutes)
+
+// Image route
+app.use("/api/uploads", uploadRoutes);
 
 app.get("/", (req, res) => {
     res.send("RescueBase Backend is now running.");
