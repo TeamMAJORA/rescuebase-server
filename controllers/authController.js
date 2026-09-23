d45571dbdd08b735c1f05addeab5833fb195863f
ShikoShintaro <shikoshintaro21@gmail.com>
2026-09-23 22:07:19 +0800

    Fix formatting inconsistencies in authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,7 @@ const cleanUser = require("../utils/cleanUser");
 const generateToken = require("../utils/generateToken");
 
 const {
-    sendOtpEmail, sendPasswordResetEmail
+     sendOtpEmail, sendPasswordResetEmail
 } = require("../services/emailService");
 
 exports.emailSignup = async (req, res) => {
@@ -428,7 +428,7 @@ exports.forgotPassword = async (req, res) => {
         user.email,
         resetOtp
     );
-    
+
     return res.status(200).json({
         success: true,
         message:
